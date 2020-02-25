f303c4dafb92e3c657325bcc35e85ae2e0e5426f
Seeroco <Seeroco@users.noreply.github.com>
2020-02-24 22:43:02 -0300

    Co

---
+++ source/repos/Golden Sun 2/Golden Sun 2/Services/SkillServices.cs
@@ -0,0 +1,23 @@
+﻿using Golden_Sun_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Golden_Sun_2.Services
+{
+    public class SkillServices
+    {
+        public List<Skill> GetSkills()
+        {
+            DBConnector db = new DBConnector();
+            SqlDataReader reader = db.connect("SELECT * FROM skills");
+            while(reader.Read())
+            {
+                reader["name"];
+            }
+
+        }
+    }
+}
