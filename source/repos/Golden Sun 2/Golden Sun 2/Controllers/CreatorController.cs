8dda7c49b134f9d5c2355201161695d21155a58a
Seeroco <Seeroco@users.noreply.github.com>
2020-02-24 22:49:29 -0300

    Cambiadou

---
+++ source/repos/Golden Sun 2/Golden Sun 2/Controllers/CreatorController.cs
@@ -0,0 +1,34 @@
+﻿using Golden_Sun_2.Models;
+using Golden_Sun_2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Golden_Sun_2.Controllers
+{
+    public class CreatorController : Controller
+    {
+        // GET: Creator
+        public ActionResult Index()
+        {
+            ViewBag.Message = "The creator page";
+            CharacterServices cs = new CharacterServices();
+            List<Character> characters = cs.GetAllCharacters();
+            List<string> charNames = new List<string>();
+            foreach (Character character in characters)
+            {
+                charNames.Add(character.Name);
+            }
+            return View("Index", charNames);
+        }
+
+        public ActionResult GetCharacter(string charName)
+        {
+            var cs = new CharacterServices();
+            var pj = cs.GetCharacter(charName);
+            return View("GetCharacter", pj);
+        }
+    }
+}
---
+++ source/repos/Golden Sun 2/Golden Sun 2/Services/ItemServices.cs
@@ -0,0 +1,37 @@
+﻿using Golden_Sun_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Golden_Sun_2.Services
+{
+    public class ItemServices
+    {
+        public List<Item> GetItems(List<int> itemsID)
+        {
+            DBConnector db = new DBConnector();
+            List<Item> allItems = new List<Item>();
+            var itemReader = db.connect("SELECT * FROM longswords");
+            while (itemReader.Read())
+            {
+                allItems.Add(new Item(int.Parse(itemReader["ID"].ToString()),
+                                             itemReader["Name"].ToString(),
+                                             int.Parse(itemReader["Stat"].ToString()),
+                                             int.Parse(itemReader["Cost"].ToString()),
+                                             itemReader["is_rare"].ToString().Equals("True")));
+
+            }
+            List<Item> items = new List<Item>();
+            for (int i = 0; i < itemsID.Count; i++)
+            {
+                items.Add(allItems.Find(item => item.ID == itemsID[i]));
+            }
+            
+            return items;
+            //Hello Itemsss
+
+        }
+    }
+}
